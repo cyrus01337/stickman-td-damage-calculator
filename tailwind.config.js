582b2a4515c84e13b1a74bc77c7cee5db08e151e
cyrus01337 <cyrus01337@gmail.com>
2025-02-15 02:06:40 +0000

    Ensure mobile compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,21 @@ export default {
         themes: ["light"],
     },
     theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+        },
         extend: {
             fontFamily: {
                 sans: ["Inter Tight Variable", ...defaultTheme.fontFamily.sans],
             },
+        },
+        screens: {
+            "2xl": { max: "1535px" },
+            xl: { max: "1279px" },
+            lg: { max: "1023px" },
+            md: { max: "767px" },
+            sm: { max: "639px" },
         },
     },
     plugins: [daisyUI],
