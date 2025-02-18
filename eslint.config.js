fee79d3e6e71825009ae33dcb4d09e7985efb862
cyrus01337 <cyrus01337@gmail.com>
2025-02-18 10:43:59 +0000

    Support linting React

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import javascript from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import astro from "eslint-plugin-astro";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 import typescript from "typescript-eslint";
 
 export default typescript.config(
@@ -9,6 +12,57 @@ export default typescript.config(
     ...typescript.configs.recommended,
     ...typescript.configs.stylistic,
     ...astro.configs.recommended,
+    {
+        ...react.configs.flat.recommended,
+        ...react.configs.flat["jsx-runtime"],
+
+        files: ["**/*.{jsx,tsx}"],
+        languageOptions: {
+            ...react.configs.flat.recommended.languageOptions,
+            ...react.configs.flat["jsx-runtime"].languageOptions,
+
+            globals: {
+                ...globals.browser,
+                ...globals.serviceworker,
+            },
+        },
+        rules: {
+            ...react.configs.flat.recommended.rules,
+            ...react.configs.flat["jsx-runtime"].rules,
+
+            "react/display-name": "off",
+            "react/no-unknown-property": "error",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    {
+        plugins: {
+            "react-hooks": reactHooks,
+        },
+        rules: reactHooks.configs.recommended.rules,
+    },
+    {
+        ignores: [".astro/", ".husky/", "dist/", "external/", "node_modules/"],
+    },
+    {
+        rules: {
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-unsafe-assignment": "off",
+            "@typescript-eslint/no-unsafe-call": "off",
+            "@typescript-eslint/no-unsafe-member-access": "off",
+            "@typescript-eslint/no-unused-vars": "error",
+            "@typescript-eslint/triple-slash-reference": "off",
+            "import/no-anonymous-default-export": "off",
+            "no-console": "warn",
+            "no-control-regex": "off",
+        },
+    },
+
     {
         ignores: ["dist/", "external/", "node_modules/", ".astro/", ".husky/"],
     },
